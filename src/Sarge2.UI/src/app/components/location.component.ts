3a13dc62bbbeb8ea3c82e950f28fcabdcd1eb1c7
Roar Fredriksen <roarfred@gmail.com>
2017-11-25 16:12:37 +0100

    Fixed editing of position

--- src/Sarge2.UI/src/app/components/location.component.ts
+++ src/Sarge2.UI/src/app/components/location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 import { Location } from '../models/location.model';
 
 declare var proj4:any;
@@ -10,7 +10,9 @@ declare var proj4:any;
 })
 export class LocationComponent  { 
     _location : Location;
-
+    editEasting: string;
+    editNorthing: string;
+    
     @Input() 
     get location() : Location {
         return this._location;
@@ -21,4 +23,21 @@ export class LocationComponent  {
     }
     @Output()
     locationChange = new EventEmitter();
+
+    @Input()
+    public editMode = false;
+    @Input()
+    public editable = false;
+    edit() : void {
+        this.editMode = !this.editMode;
+        if (this.editMode)
+        {
+            this.editEasting = this.location.getLocalLocation().easting.toFixed(0);
+            this.editNorthing = this.location.getLocalLocation().northing.toFixed(0);
+        }
+        else
+        {
+            this.location = new Location(this.location.getLocalLocation().zone, parseFloat(this.editEasting), parseFloat(this.editNorthing)).getLocation(this.location.zone);
+        }
+    }
 }
--- src/Sarge2.UI/src/app/pipes/utm-location.pipe.ts
+++ src/Sarge2.UI/src/app/pipes/utm-location.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'utmLocation' })
 export class UtmLocationPipe implements PipeTransform {
     transform(value: number): string {
-        return this.pad(Math.floor(value), 7);
+        return this.pad(Math.round(value), 7);
     }
 
     pad(value: number, digits: number) {
