1c706a808493298781b3716023e37bec518ab29d
Roar Fredriksen <roarfred@gmail.com>
2017-12-24 16:11:20 +0100

    Testing camera capture

--- src/Sarge2.UI/src/app/components/menus/menu-buttons.component.ts
+++ src/Sarge2.UI/src/app/components/menus/menu-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-menu-buttons',
@@ -6,6 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./menu-buttons.component.css']
 }) export class MenuButtonsComponent {
     public menuName: string;
+    @ViewChild('photo') photo: any;
     public menuItems: Array<any> = [
         {name: 'print', icon: 'print'},
         {name: 'location', icon: 'info_outline'},
@@ -13,12 +14,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
         {name: 'tracks', icon: 'gesture'},
         {name: 'pois', icon: 'flag'},
         {name: 'areas', icon: 'format_shapes'},
-        {name: 'import', icon: 'backup'}
+        {name: 'import', icon: 'backup'},
+        {name: 'camera', icon: 'photo_camera', action: this.capturePhoto}
     ];
     @Output()
     public menuClick = new EventEmitter<string>();
     public toggleMenu(menuName: string): void {
         this.menuName = menuName;
         this.menuClick.emit(menuName);
+    }
+    public click(item) {
+        if (item.action) {
+            item.action(item, this);
+        } else {
+            this.toggleMenu(item.name);
+        }
+    }
+    public capturePhoto(item, component): void {
+        component.photo.nativeElement.click();
     }
 }
